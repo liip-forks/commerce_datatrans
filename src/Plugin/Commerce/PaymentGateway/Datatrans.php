ba9757777ac31d03e7bd1a9eb9745c91e2740fb6
Primsi <primsi@gmail.com>
2017-02-06 06:41:41 -0800

    by Primsi: basic module structure.

---
+++ src/Plugin/Commerce/PaymentGateway/Datatrans.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\commerce_datatrans\Plugin\Commerce\PaymentGateway;
+
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Provides the Datatrans payment gateway.
+ *
+ * @CommercePaymentGateway(
+ *   id = "datatrans",
+ *   label = "Datatrans",
+ *   display_label = "Datatrans",
+ *    forms = {
+ *     "offsite-payment" = "Drupal\commerce_datatrans\PluginForm\Datatrans\DatatransForm",
+ *   },
+ *   payment_method_types = {"credit_card"},
+ *   credit_card_types = {
+ *     "amex", "dinersclub", "discover", "jcb", "maestro", "mastercard", "visa",
+ *   },
+ * )
+ */
+class Datatrans extends OffsitePaymentGatewayBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'redirect_method' => 'post',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    // A real gateway would always know which redirect method should be used,
+    // it's made configurable here for test purposes.
+    $form['redirect_method'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Redirect method'),
+      '#options' => [
+        'get' => $this->t('Redirect via GET (302 header)'),
+        'post' => $this->t('Redirect via POST'),
+      ],
+      '#default_value' => $this->configuration['redirect_method'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    if (!$form_state->getErrors()) {
+      $values = $form_state->getValue($form['#parents']);
+      $this->configuration['redirect_method'] = $values['redirect_method'];
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onReturn(OrderInterface $order, Request $request) {
+    // @todo Add examples of request validation.
+    $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+    $payment = $payment_storage->create([
+      'state' => 'authorization',
+      'amount' => $order->getTotalPrice(),
+      'payment_gateway' => $this->entityId,
+      'order_id' => $order->id(),
+      'test' => $this->getMode() == 'test',
+      'remote_id' => $request->query->get('txn_id'),
+      'remote_state' => $request->query->get('payment_status'),
+      'authorized' => REQUEST_TIME,
+    ]);
+    $payment->save();
+    drupal_set_message('Payment was processed');
+  }
+
+}
