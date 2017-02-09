4abc47cddbd18348a336c3b8735ae1df83663c61
Primsi <primsi@gmail.com>
2017-02-09 09:14:10 -0800

    by Primsi: remove helper generateDatatransKey because no longer needed.

--- src/DatatransHelper.php
+++ src/DatatransHelper.php
@@ -12,26 +12,6 @@ use Drupal\Core\Site\Settings;
 class DatatransHelper {
 
   /**
-   * Used for generation an internal key for validation purposes.
-   *
-   * @param PaymentInterface $payment
-   *   Payment Interface.
-   *
-   * @return string
-   *   The generated key.
-   */
-  public static function generateDatatransKey(PaymentInterface $payment) {
-    $hash_parts = [
-      $payment->id(),
-      $payment->getPaymentGatewayId(),
-      Settings::getHashSalt(),
-      \Drupal::service('private_key')->get(),
-    ];
-
-    return Crypt::hashBase64(implode('', $hash_parts));
-  }
-
-  /**
    * Generates the server side sign to compare with the datatrans post data.
    *
    * @param string $hmac_key
